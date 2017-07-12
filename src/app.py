f624ca0189cf180ada98518d5dc7c05feefbed88
Jushita Rahman <jushitaa@gmail.com>
2017-07-12 11:06:46 -0400

    skeleton of page

--- src/app.py
+++ src/app.py
@@ -4,11 +4,13 @@ from flask import Flask, render_template, request, session, make_response
 
 
 app = Flask(__name__)  # '__main__'
+app.secret_key = 'development key'
 
 
 @app.route('/')
 def home_template():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.debug = True
